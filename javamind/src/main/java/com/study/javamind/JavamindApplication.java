d895309b0cb6829efcb508698c838149d0e22770
zou_xq <zou_xq@haici.com>
2020-08-27 11:40:47 +0800

    首次提交

---
+++ javamind/src/main/java/com/study/javamind/JavamindApplication.java
@@ -0,0 +1,13 @@
+package com.study.javamind;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavamindApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavamindApplication.class, args);
+    }
+
+}

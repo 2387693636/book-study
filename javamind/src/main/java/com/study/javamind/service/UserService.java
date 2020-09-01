89c2bef216d851636a0d9eac80ca3090a818f630
zou_xq <zou_xq@haici.com>
2020-09-01 16:27:04 +0800

    新增代码提交

--- javamind/src/main/java/com/study/javamind/service/UserService.java
+++ javamind/src/main/java/com/study/javamind/service/UserService.java
@@ -1,11 +1,7 @@
 package com.study.javamind.service;
 
 import com.study.javamind.entity.UserEntity;
-import com.study.javamind.param.UserParam;
-import com.study.javamind.vo.UserVo;
 import org.apache.catalina.User;
-
-import java.util.List;
 
 /**
  * @author: zou_xq
@@ -13,7 +9,5 @@ import java.util.List;
  * @date: 2020/8/28 14:12
  */
 public interface UserService {
-    String getUser(UserEntity user);
-
-    List<UserVo> getUserInfo(UserParam user);
+    public String getUser(UserEntity user);
 }
